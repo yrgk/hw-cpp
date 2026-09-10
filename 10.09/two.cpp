af908167a3b0e125611ea1a83e14347603cad597
Yar <yarik090908@gmail.com>
2026-09-10 12:42:43 +0300

    made hw for 10.09

---
+++ 10.09/two.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    while (1)
+    {
+        do
+        {
+            1;
+            2;
+        } while (2);
+
+        do
+        {
+            if (3)
+            {
+                if (4)
+                {
+                    3;
+                }
+            }
+            else
+            {
+                4;
+            }
+        } while (4);
+    }
+}
